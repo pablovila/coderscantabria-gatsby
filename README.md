6206ba6c04b84b9091a3cda200a14f3f68b9cadc
Pablo Vila <pablo.vilafer@gmail.com>
2019-02-24 21:43:28 +0100

    initial release

---
+++ README.md
@@ -0,0 +1,9 @@
+# gatsby-starter-wordpress-community
+
+Demo: https://gatsby-starter-wordpress-community.netlify.com/
+
+## Use it now
+
+    gatsby new NAME https://github.com/pablovila/gatsby-starter-wordpress-community
+
+* Edit `gatsby-config.js`, change `baseUrl`
---
+++ gatsby-config.js
@@ -0,0 +1,54 @@
+require("dotenv").config({
+  path: `.env`
+});
+
+module.exports = {
+  siteMetadata: {
+    title: `CodersCantabria`,
+    description: `Comunidad de Programación y Tecnología en Cantabria`,
+    author: `@pablovilafer`
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`
+      }
+    },
+    {
+      resolve: "gatsby-source-wordpress",
+      options: {
+        baseUrl: "coderscantabria.com",
+        protocol: "https",
+        hostingWPCOM: true,
+        useACF: false,
+        auth: {
+          wpcom_app_clientId: process.env.WORDPRESS_CLIENT_ID,
+          wpcom_app_clientSecret: process.env.WORDPRESS_CLIENT_SECRET,
+          wpcom_user: process.env.WORDPRESS_USER,
+          wpcom_pass: process.env.WORDPRESS_PASSWORD
+        }
+      }
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-purgecss`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `gatsby-starter-default`,
+        short_name: `starter`,
+        start_url: `/`,
+        background_color: `#663399`,
+        theme_color: `#663399`,
+        display: `minimal-ui`,
+        icon: `src/images/logo.svg` // This path is relative to the root of the site.
+      }
+    },
+    "gatsby-plugin-offline",
+    `gatsby-plugin-netlify` // make sure to put last in the array
+  ]
+};
